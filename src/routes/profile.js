6ae30ad8639f65bc9e0022271b82197e62b87ece
Adil Hashmi <hashmiadil161@gmail.com>
2024-12-09 13:52:42 +0530

    add edit-password patch request

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -2,6 +2,8 @@ const express = require("express");
 const profileRouter = express.Router();
 const { userAuth } = require("../middlewares/auth");
 const { validateEditProfileData } = require("../utils/validation");
+const bycrypt = require("bcrypt");
+const validator = require("validator");
 
 profileRouter.get("/profile/view", userAuth, async (req, res) => {
   try {
@@ -25,6 +27,33 @@ profileRouter.patch("/profile/edit", userAuth, async (req, res) => {
       message: `${loggedInUser.firstName} profile edited successfully`,
       data: loggedInUser,
     });
+  } catch (err) {
+    res.status(400).send("Error : " + err.message);
+  }
+});
+
+profileRouter.patch("/profile/edit-password", userAuth, async (req, res) => {
+  try {
+    const { currentPassword, newPassword } = req.body;
+
+    const isCurrentPasswordValid = await req.user.validatePassword(
+      currentPassword
+    );
+
+    if (!isCurrentPasswordValid) {
+      throw new Error("Invalid password");
+    }
+
+    if (!validator.isStrongPassword(newPassword)) {
+      throw new Error(
+        "Password should be minimum 8 character long and should contain atleast 1 lowercase, 1 uppercase, l number, and 1 symbol."
+      );
+    }
+
+    const newHashedPassword = await bycrypt.hash(newPassword, 10);
+    req.user.password = newHashedPassword;
+    await req.user.save();
+    res.send("Password Updated Successfully");
   } catch (err) {
     res.status(400).send("Error : " + err.message);
   }
